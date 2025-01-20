601213e966924bf1c73c2e18f33ce7183d8c8eab
Demiurg <danzopoi228@mail.ru>
2025-01-20 16:48:03 +0300

    created insight-section (without adaptive)

---
+++ src/js/components/slider.js
@@ -0,0 +1,12 @@
+import Swiper from "swiper";
+import 'swiper/css';
+
+export const useInsightSlider = () => {
+    new Swiper(
+        '.insight__slider', {
+            slidesPerView: "auto",
+            spaceBetween: 32,
+            loop: true,
+        }
+    );
+};
